4679db2d074bf51cdcdcb9d756b05562be30baef
Tanishkagupta123 <tanishkagupta241@gmail.com>
2026-03-24 01:48:18 +0530

    update

--- project/app/views.py
+++ project/app/views.py
@@ -19,3 +19,34 @@ def all_data(req):
     data1 = Student.objects.all()
     serializer = StuSerializer(data1, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET','PUT','PATCH','DELETE'])
+def data(req,pk):
+    data=Student.objects.filter(id=pk)
+    if data:
+        if req.method=='PUT':
+            snippet = Student.objects.get(pk=pk)
+            serializer = StuSerializer(snippet, data=req.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        elif req.method=='PATCH':
+            snippet = Student.objects.get(pk=pk)
+            serializer = StuSerializer(snippet, data=req.data,partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        elif req.method=='DELETE':
+            snippet = Student.objects.get(pk=pk)
+            snippet.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        elif req.method=='GET':
+            snippet = Student.objects.get(pk=pk)
+            serializer = StuSerializer(snippet)
+            return Response(serializer.data)
+    data={"msg":"Id is not present in DB"}
+    return Response(data)
+    
--- project/project/urls.py
+++ project/project/urls.py
@@ -20,7 +20,7 @@ from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alldata/',all_data),
-    
+    path('data/',all_data),
+    path('data/<int:pk>',data),
 
 ]
